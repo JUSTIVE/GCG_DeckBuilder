f55f5fe63075ea0891cebd32cdb28a97394e52fc
Ben <ben399399@naver.com>
2026-03-16 13:04:59 +0900

    feat: wip on names

---
+++ data/2.mapper.ts
@@ -0,0 +1,45 @@
+import raw from "./raw.json";
+import { writeFile } from "node:fs/promises";
+import effects from "./effects.json";
+import names from "./names.json";
+
+const nameEntries = Object.entries(names);
+const effectEntries = Object.entries(effects);
+const allEntries = [...effectEntries, ...nameEntries];
+
+function maps(target: string[], kvMap: [string, string][]): string {
+  return kvMap.reduce((acc, [k, v]) => acc.replaceAll(k, v), target);
+}
+
+const mapped = raw
+  .map((x) => {
+    if (x.description == null) return x;
+
+    return {
+      ...x,
+      description: x.description.map((x) => maps(x, allEntries)),
+    };
+  })
+  .map((x) => {
+    if (x.name == null) return x;
+    return {
+      ...x,
+      name: maps(x.name, nameEntries),
+    };
+  })
+  .map((x) => {
+    if (x?.link?.pilotName == null) return x;
+    return {
+      ...x,
+      link: {
+        ...x.link,
+        pilotName: maps(x.link.pilotName, nameEntries),
+      },
+    };
+  });
+
+await writeFile(
+  "../data/mapped.json",
+  JSON.stringify(mapped, null, 2),
+  "utf-8",
+);
