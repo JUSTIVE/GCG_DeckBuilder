985dd35a21d96abc8d1be0ca6217ffc41e3a6515
JUSTIVE <ben399399@naver.com>
2026-03-30 04:35:43 +0900

    fix: BaseCard 밀림수정

--- frontend/src/components/BaseCard.tsx
+++ frontend/src/components/BaseCard.tsx
@@ -90,7 +90,7 @@ export function BaseCardBody({
           </div>
           <div
             className={cn(
-              "w-[3.5cqw] cutout cutout-br-lg h-[15cqw] font-semibold pl-0.5 flex items-center -translate-y-[8cqw]",
+              "w-[3.5cqw] cutout cutout-br-lg h-[15cqw] font-semibold pl-0.5 flex items-center -mt-[8cqw]",
               COLOR_BG[baseCard.color],
             )}
           />
