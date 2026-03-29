a4173650dbf5f980cd5d7c51603f0ebda9ac0ee6
JUSTIVE <ben399399@naver.com>
2026-03-29 16:13:49 +0900

    removed menus

--- frontend/src/lib/nav.ts
+++ frontend/src/lib/nav.ts
@@ -8,57 +8,11 @@ export type NavItem = {
 export const navMain: NavItem[] = [
   {
     title: "카드 찾기",
-    url: "/cardlist",
+    url: "",
     items: [
-      { title: "Installation", url: "/" },
-      { title: "Project Structure", url: "#" },
+      { title: "전체 카드", url: "/cardlist" },
+      { title: "스타터 덱", url: "#" },
     ],
-  },
-  {
-    title: "Build Your Application",
-    url: "#",
-    items: [
-      { title: "Routing", url: "#" },
-      { title: "Data Fetching", url: "#", isActive: true },
-      { title: "Rendering", url: "#" },
-      { title: "Caching", url: "#" },
-      { title: "Styling", url: "#" },
-      { title: "Optimizing", url: "#" },
-      { title: "Configuring", url: "#" },
-      { title: "Testing", url: "#" },
-      { title: "Authentication", url: "#" },
-      { title: "Deploying", url: "#" },
-      { title: "Upgrading", url: "#" },
-      { title: "Examples", url: "#" },
-    ],
-  },
-  {
-    title: "API Reference",
-    url: "#",
-    items: [
-      { title: "Components", url: "#" },
-      { title: "File Conventions", url: "#" },
-      { title: "Functions", url: "#" },
-      { title: "next.config.js Options", url: "#" },
-      { title: "CLI", url: "#" },
-      { title: "Edge Runtime", url: "#" },
-    ],
-  },
-  {
-    title: "Architecture",
-    url: "#",
-    items: [
-      { title: "Accessibility", url: "#" },
-      { title: "Fast Refresh", url: "#" },
-      { title: "Next.js Compiler", url: "#" },
-      { title: "Supported Browsers", url: "#" },
-      { title: "Turbopack", url: "#" },
-    ],
-  },
-  {
-    title: "Community",
-    url: "#",
-    items: [{ title: "Contribution Guide", url: "#" }],
   },
 ];
 
