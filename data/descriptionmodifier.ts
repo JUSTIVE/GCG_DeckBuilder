ff68e218b85ce5552524180fa004bc507e9eb46b
JUSTIVE <ben399399@naver.com>
2026-04-04 01:39:51 +0900

    feat: 3스트

--- data/descriptionmodifier.ts
+++ data/descriptionmodifier.ts
@@ -5,6 +5,8 @@ import descriptionModifier from "./descriptionmap.json";
 const replaceList = {
   "】": "】 ",
   "  ": " ",
+  "(Earth Federation)": "(지구 연방)",
+  "(Neo Zeon)": "(네오 지온)",
   "(Zeon)": "(지온)",
   "(Coordinator)": "(코디네이터)",
   "(ZAFT)": "(자프트)",
@@ -33,6 +35,11 @@ const replaceList = {
   "Choose 1 to 2 friendly (Clan) Units.": "(클랜)의 아군의 유닛 1~2개를 선택한다.",
   "They get AP+2 during this turn.": "이 턴 동안, 그 유닛들을 AP+2 한다.",
   "If you have a (Clan) Unit in play,": "(클랜)의 자신의 유닛이 있다면,",
+  "Then, if it is your turn,": "그 후, 자신의 턴 이라면,",
+  "It can't receive battle damage from enemy Units with 2 or less AP during this battle.":
+    "이 배틀 동안, 그 유닛은 AP2 이하의 상대의 유닛으로부터 배틀 대미지를 받지 않는다.",
+  "Return the remaining cards randomly to the bottom of your deck.":
+    "남은 카드는 무작위로 덱의 밑으로 되돌린다.",
 };
 
 function applyReplacements(v: string): string {
