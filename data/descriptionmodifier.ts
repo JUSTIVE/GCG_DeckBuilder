3af0051013aacc163c02f043f02bd60d9df34832
JUSTIVE <ben399399@naver.com>
2026-04-04 01:16:52 +0900

    wip

--- data/descriptionmodifier.ts
+++ data/descriptionmodifier.ts
@@ -8,6 +8,7 @@ const replaceList = {
   "(Zeon)": "(지온)",
   "(Coordinator)": "(코디네이터)",
   "(ZAFT)": "(자프트)",
+  "(Triple Ship Alliance)": "(삼척동맹)",
   "this Unit gets AP+2 during this battle.": "이 배틀 동안, 이 유닛을 AP+2 한다.",
   "If you are attacking the enemy player,": "상대 플레이어를 공격하고 있다면,",
   "Choose 1 enemy Unit with 5 or less HP.": "HP5 이하의 상대의 유닛 1개를 선택한다.",
@@ -25,6 +26,9 @@ const replaceList = {
   "It gets AP+2 during this turn.": "이 턴 동안, 그 유닛을 AP+2 한다.",
   "It recovers 1 HP.": "그 유닛을 1 회복한다.",
   "Choose 1 enemy Unit with 2 or less HP.": "HP2 이하의 상대의 유닛 1개를 선택한다.",
+  "It gets AP-1 during this turn.": "이 턴 동안, 그 유닛을 AP-1 한다.",
+  "Add it to your hand.": "그 카드를 패에 추가한다.",
+  "Choose 1 enemy Unit that is Lv.2 or lower.": "Lv.2 이하의 상대의 유닛 1개를 선택한다.",
 };
 
 function applyReplacements(v: string): string {
