548269013963b342ae01bf7ee778ca776fec6259
JUSTIVE <ben399399@naver.com>
2026-04-04 00:39:36 +0900

    wip

--- data/descriptionmodifier.ts
+++ data/descriptionmodifier.ts
@@ -2,8 +2,12 @@ import { writeFile } from "node:fs/promises";
 
 import descriptionModifier from "./descriptionmap.json";
 
-const replaceList: {
-  "(Zeon)": "(지온)";
+const replaceList = {
+  "(Zeon)": "(지온)",
+  "(Coordinator)": "(코디네이터)",
+  "(ZAFT)": "(자프트)",
+  "this Unit gets AP+2 during this battle.": "이 배틀 동안, 이 유닛을 AP+2 한다.",
+  "If you are attacking the enemy player,": "상대 플레이어를 공격하고 있다면,",
 };
 
 function applyReplacements(v: string): string {
@@ -14,7 +18,7 @@ function applyReplacements(v: string): string {
 }
 
 const modifiedDescriptionModifier = Object.fromEntries(
-  Object.fromEntries(descriptionModifier).map(([k, v]) => [k, applyReplacements(v)]),
+  Object.entries(descriptionModifier).map(([k, v]) => [k, applyReplacements(v)]),
 );
 
 await writeFile("descriptionmap.json", JSON.stringify(modifiedDescriptionModifier, null, 2));
